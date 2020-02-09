83729659ed210084ecdaf5895b6e91c33f076735
Nick Irvine <nfirvine@nfirvine.com>
2020-02-09 12:24:04 -0800

    add terminal

--- keyboards/keebio/quefrency/rules.mk
+++ keyboards/keebio/quefrency/rules.mk
@@ -30,6 +30,8 @@ RGBLIGHT_ENABLE = yes        # Enable WS2812 RGB underlight.
 # Do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 SLEEP_LED_ENABLE = no    # Breathing sleep LED during USB suspend
 
+TERMINAL_ENABLE = yes
+
 SPLIT_KEYBOARD = yes
 
 DEFAULT_FOLDER = keebio/quefrency/rev1
